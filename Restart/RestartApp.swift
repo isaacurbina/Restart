4a4d455d3fd71c7bd12e804d7582bdcc1d0af975
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-03-24 17:41:07 -0500

    first commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Isaac Urbina on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
