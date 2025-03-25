3707489414e570e09bb200109d2f1a3a151961b4
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-03-25 15:21:03 -0500

    66. 3. Let's develop the Home Screen with SwiftUI

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -18,14 +18,46 @@ struct HomeView: View {
 	
     var body: some View {
 		VStack(spacing: 20) {
-			Text("Home")
-				.font(.largeTitle)
 			
+			
+			// MARK: - header
+			
+			Spacer()
+			ZStack {
+				CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+				Image("character-2")
+					.resizable()
+					.scaledToFit()
+					.padding()
+			}
+			
+			
+			// MARK: - center
+			
+			Text("The time that leads to mastery is dependent on the intensity of our focus.")
+				.font(.title3)
+				.fontWeight(.light)
+				.foregroundColor(.secondary)
+				.multilineTextAlignment(.center)
+				.padding()
+			
+			
+			// MARK: - footer
+			
+			Spacer()
 			Button {
 				isOnboardingViewActive = true
 			} label: {
+				Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+					.imageScale(.large)
+				
 				Text("Restart")
+					.font(.system(.title3, design: .rounded))
+					.fontWeight(.bold)
 			}
+			.buttonStyle(.borderedProminent)
+			.buttonBorderShape(.capsule)
+			.controlSize(.large)
 		}
     }
 }
